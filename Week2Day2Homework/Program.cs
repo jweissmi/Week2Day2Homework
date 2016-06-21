d5be0802f9667464f864af100f6b475676762325
Jim Weiss <jweiss@motor.com>
2016-06-21 16:26:26 -0400

    Update Version 1

--- Week2Day2Homework/Program.cs
+++ Week2Day2Homework/Program.cs
@@ -10,6 +10,104 @@ namespace Week2Day2Homework
     {
         static void Main(string[] args)
         {
+            Animal A;
+            A = new Animal("Cat", "Scales");
+            //Type t = A.GetType();
+            Console.WriteLine("My animal is a: " + A.Species);
+            Console.WriteLine("My animal is covered with: " + A.ExternalCovering);
+
+            Mammal P;
+            P = new Mammal(1);
+            //Type t = A.GetType();
+            Console.WriteLine("My Mammal has " + P.AvgNumOffspring + " average number of offstring.");
+          
+            Console.ReadLine();
+
+        }
+    }
+    public class Animal
+    {
+        private string _Species;
+        private string _ExternalCovering;
+
+        public Animal()
+        {
+        }
+        public Animal(string _Species, string _ExternalCovering)
+        {
+            this.Species = _Species;
+            this.ExternalCovering = _ExternalCovering;
+        }
+
+        public string Species
+        {
+            get
+            {
+                return _Species;
+            }
+            set
+            {
+                _Species = value;
+            }
+        }
+        public string ExternalCovering
+        {
+            get
+            {
+                return _ExternalCovering;
+            }
+            set
+            {
+                _ExternalCovering = value;
+            }
+        }
+
+        public virtual string GetDisplayText(string sep) =>
+            Species + sep + ExternalCovering;
+
+    }
+    public class Mammal : Animal
+    {
+        private int _AvgNumOffspring;
+
+        public Mammal()
+        {
+        }
+        public Mammal(int _AvgNumOffspring)
+        {
+            AvgNumOffspring = _AvgNumOffspring;
+        }
+
+        public int AvgNumOffspring
+        {
+            get
+            {
+                return _AvgNumOffspring;
+            }
+            set
+            {
+                _AvgNumOffspring = value;
+            }
+        }
+        public class Reptile : Animal
+        {
+
+        }
+        public class Human : Mammal
+        {
+
+        }
+        public class Dog : Mammal
+        {
+
+        }
+        public class Snake : Reptile
+        {
+
+        }
+        public class Lizard : Reptile
+        {
+
         }
     }
 }
